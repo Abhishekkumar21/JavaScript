30999f789bf9bcc7e79e1457961448cba3f8631f
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2023-02-13 21:20:59 +0530

    'Fuction in JS' file added

---
+++ 01_Fundamentals.js
@@ -0,0 +1,17 @@
+
+
+//Function declaration
+
+function message (){
+    alert("Hello! This is message() function body statement.");
+ }
+message();  // function calling 
+
+
+ // Function expression
+
+let message1 = function (){
+    alert("Hello! This is message1() function body statement.");
+}
+message1(); // function calling.
+
